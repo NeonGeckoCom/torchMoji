d33276bc7965aaf2ccfdb55042c3d2e4c1717730
Daniel McKnight <daniel@neongecko.com>
2020-12-18 12:59:32 -0800

    Update numpy/scipy packages for chatbots compat

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ setup(
     include_package_data=True,
     install_requires=[
         'emoji==0.4.5',
-        'numpy==1.13.1',
-        'scipy==0.19.1',
+        'numpy~=1.18.5',
+        'scipy~=1.5.4',
         'scikit-learn==0.19.0',
         'text-unidecode==1.0',
+        'nose~=1.3.7',
+        'torch~=1.7.1'
     ],
 )
