1ae147efeb9775895cbc6b7b8ab691369cbcf273
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2023-11-30 19:10:39 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         'emoji~=0.4',
         'numpy~=1.18',
         'scipy~=1.5',
-        'scikit-learn==0.19.0',
+        'scikit-learn~=0.19',
         'text-unidecode==1.0',
         'nose~=1.3.7',
         'torch~=1.7.1'
